b3ceeabe69eebd8d99345774a5c96a04f3815072
ZhaoSQ <1044239768@qq.com>
2021-05-18 16:01:18 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,44 @@
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
-void hardware_Init(void) {
+static void led_Init(void);
+static void hardware_Init(void);
+static void led_Blink(void);
+
+static void led_Init(void) {
+    //< P1.0 Init
+    GPIO_setAsOutputPin(
+            GPIO_PORT_P1,
+            GPIO_PIN0
+            );
+}
+
+static void hardware_Init(void) {
     //< stop watchdog timer
     WDT_A_hold(WDT_A_BASE);
+    led_Init();
 }
 
-void dummy (void) {
+static void led_Blink(void) {
+    volatile uint32_t i;
+
+    // Toggle P1.0 output
+    GPIO_toggleOutputOnPin(
+       GPIO_PORT_P1,
+       GPIO_PIN0
+       );
+
+    // Delay
+    for(i = 100000; i > 0; i--);
 }
 
-void do_Something_One(void) {
-    dummy();
+static void do_Something_One(void) {
+    led_Blink();
 }
 
-void do_Something_Two(void) {
-    dummy();
+static void do_Something_Two(void) {
+    led_Blink();
 }
+
 int main(void)
 {
     //< hardware init
