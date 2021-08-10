bbf2738eb10ca73f11a856740c7e39d21cc26805
ZhaoSQ <1044239768@qq.com>
2021-08-10 17:54:41 +0800

    增加rtos实验文件
    将原来文件增加到指定文件夹，故表现为增加文件。

---
+++ example/rtos_test/msp432p401r/lab.h
@@ -0,0 +1,24 @@
+/*
+ * lab.h
+ *
+ * Copyright (c) 2021, ZhaoSQ-CH.CaiNiaoLab
+ * Author: ZhaoSQ-CH.CaiNiaoLab
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef EXAMPLE_LAB_H_
+#define EXAMPLE_LAB_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int lab1main(void);
+int lab2main(void);
+int lab3main(void);
+int lab4main(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* EXAMPLE_LAB_H_ */
---
+++ example/rtos_test/msp432p401r/lab1.c
@@ -0,0 +1,119 @@
+/*
+ * lab1.c
+ *
+ * Copyright (c) 2021, ZhaoSQ-CH.CaiNiaoLab
+ * Author: ZhaoSQ-CH.CaiNiaoLab
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <micrortos_config.h>
+#include "lab.h"
+
+
+#if LAB_1
+static void led_RGB_Init(void);
+static void hardware_Init(void);
+
+static void led_RGB_Init(void) {
+    //< P2.0 P2.1 P2.2 Init
+    GPIO_setAsOutputPin(
+            GPIO_PORT_P2,
+            GPIO_PIN0|GPIO_PIN1|GPIO_PIN2
+            );
+    GPIO_setOutputLowOnPin(
+            GPIO_PORT_P2,
+            GPIO_PIN0|GPIO_PIN1|GPIO_PIN2
+            );
+}
+
+static void hardware_Init(void) {
+    //< stop watchdog timer
+    WDT_A_hold(WDT_A_BASE);
+    led_RGB_Init();
+}
+
+static void led_RED_Blink(void) {
+    volatile uint32_t i;
+
+    // High P2.0 output
+    GPIO_setOutputHighOnPin(
+        GPIO_PORT_P2,
+        GPIO_PIN0
+        );
+
+    // Delay
+    for(i=100000; i>0; i--);
+
+    // Low P2.0 output
+    GPIO_setOutputLowOnPin(
+        GPIO_PORT_P2,
+        GPIO_PIN0
+        );
+
+    for(i=100000; i>0; i--);
+}
+
+static void led_GREEN_Blink(void) {
+    volatile uint32_t i;
+
+    // High P2.1 output
+    GPIO_setOutputHighOnPin(
+        GPIO_PORT_P2,
+        GPIO_PIN1
+        );
+
+    // Delay
+    for(i=100000; i>0; i--);
+
+    // Low P2.1 output
+    GPIO_setOutputLowOnPin(
+        GPIO_PORT_P2,
+        GPIO_PIN1
+        );
+
+    for(i=100000; i>0; i--);
+}
+
+static void led_BLUE_Blink(void) {
+    volatile uint32_t i;
+
+    // High P2.2 output
+    GPIO_setOutputHighOnPin(
+        GPIO_PORT_P2,
+        GPIO_PIN2
+        );
+
+    // Delay
+    for(i=100000; i>0; i--);
+
+    // Low P2.2 output
+    GPIO_setOutputLowOnPin(
+        GPIO_PORT_P2,
+        GPIO_PIN2
+        );
+
+    for(i=100000; i>0; i--);
+}
+
+
+int lab1main(void){
+
+    //< hardware init
+    hardware_Init();
+
+    while(1) {
+        //< 处理事件1
+        led_RED_Blink();
+
+        //< 处理事件2
+        led_GREEN_Blink();
+
+        //< 处理事件3
+        led_BLUE_Blink();
+    }
+}
+#else
+int lab1main(void){
+    return false;
+}
+#endif
