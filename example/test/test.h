c6ff553da16a1874c38486327429f3db835fccb0
ZhaoSQ <1044239768@qq.com>
2021-09-08 18:18:11 +0800

    增加测试文件
    以后将使用test.c 和 test.h 用于demo演示和相关案例测试。

---
+++ example/test/test.h
@@ -0,0 +1,24 @@
+/*
+ * test.h
+ *
+ * Copyright (c) 2021, ZhaoSQ-CH.CaiNiaoLab
+ * Created on: 2021年9月8日
+ *     Author: ZhaoSQ-CH.CaiNiaoLab
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//< extern function
+int main_test(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* TEST_H_ */
