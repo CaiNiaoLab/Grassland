9f3f9ad68b62e747be94d2315dd3fe6985fc40c3
ZhaoSQ <1044239768@qq.com>
2021-09-08 18:08:09 +0800

    Create main.c

---
+++ example/cc3200/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ * Copyright (c) 2021, ZhaoSQ-CH.CaiNiaoLab
+ * Created on: 2021年9月8日
+ *     Author: ZhaoSQ-CH.CaiNiaoLab
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include "test.h"
+
+int main(void) {
+	main_test();
+	return 0;
+}
